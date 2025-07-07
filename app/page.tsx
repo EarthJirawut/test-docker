0f17723ec4e61de4b93705ef7fa2de57b512bcf8
Jirawut Prasert <earth25440@gmail.com>
2025-07-07 16:58:59 +0700

    Refactor HelloPage component and remove unused CSS variables

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
 const HelloPage = () => {
-  return <div>HelloPage</div>;
+  return (
+    <div className="w-screen h-screen font-bold flex justify-center items-center text-9xl text-blue-500">
+      HELLO
+    </div>
+  );
 };
 export default HelloPage;
