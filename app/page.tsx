61570386cd80918cb74c03d770aa36d0b20a19d6
EarthJirawut <jirawut.p@minerva-th.com>
2025-07-07 17:07:36 +0700

    Update HelloPage component to display 'HELLO WORLD'

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const HelloPage = () => {
   return (
     <div className="w-screen h-screen font-bold flex justify-center items-center text-9xl text-blue-500">
-      HELLO
+      HELLO WORLD
     </div>
   );
 };
