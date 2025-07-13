8c4b30fd676f7676ba63e9881868b55a6fbb5d02
EarthJirawut <jirawut.p@minerva-th.com>
2025-07-13 22:32:47 +0700

    Add .env file creation step and display DB_HOST in HelloPage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 const HelloPage = () => {
   return (
-    <div className="w-screen h-screen font-bold flex justify-center items-center text-9xl text-blue-500">
+    <div className="w-screen h-screen font-bold flex flex-col justify-center items-center text-xl text-blue-500">
       HELLO WORLD
+      <p>ENV: {process.env.DB_HOST}</p>
     </div>
   );
 };
